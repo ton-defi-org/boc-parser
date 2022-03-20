58514249ad8b5464f708c66890458c300c5aede5
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-03-20 12:15:42 +0200

    build

--- src/App.js
+++ src/App.js
@@ -20,14 +20,6 @@ function App() {
                 <p>
                     hello world
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
             </header>
         </div>
     );
