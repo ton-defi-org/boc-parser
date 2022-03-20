51708368318f2a8ded9dbcd5d94d4f13a5e72a9b
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-03-20 12:18:56 +0200

    build

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
                 <p>
                     hello world
                 </p>
