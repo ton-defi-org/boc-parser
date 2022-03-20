9308ce3358a89f895a8e018d1c053337dd68a38a
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-03-20 13:57:57 +0200

    ton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-// global.Buffer = require("buffer/").Buffer
-//
-// const Address = require("ton").Address
-// const Cell = require("ton").Cell
-//
-// const address = new Address(0, new Buffer("EQDjVXa_oltdBP64Nc__p397xLCvGm2IcZ1ba7anSW0NAkeP"))
-// const cell = new Cell();
-// cell.bits.writeUint(0, 32);
-// cell.bits.writeAddress(address);
-// const result = cell.toString()
+global.Buffer = require("buffer/").Buffer
+
+const Address = require("ton").Address
+const Cell = require("ton").Cell
+
+const address = new Address(0, new Buffer("EQDjVXa_oltdBP64Nc__p397xLCvGm2IcZ1ba7anSW0NAkeP"))
+const cell = new Cell();
+cell.bits.writeUint(0, 32);
+cell.bits.writeAddress(address);
+const result = cell.toString()
 
 function App() {
 
@@ -16,7 +16,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <p>
-                    hello world
+                    {result}
                 </p>
             </header>
         </div>
