fc4020ea24926bdc328c0f83561c7ba94516262d
ordd <or.dd100@gmail.com>
2022-03-28 20:46:56 +0300

    added changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useCallback, useRef } from 'react';
+import BigNumber from "bignumber.js";
 import './App.css';
 
 const Cell = require("ton").Cell
@@ -23,12 +24,11 @@ function App() {
                     const slice = myCell[0].beginParse();
                     const cellRef = slice.readRef();
                     const flags = cellRef.readUint(6);  // flags
-                    let address = cellRef.readAddress();  // destation address
-                    let amount = cellRef.readCoins()   // amount
-                    const result = flags.toString(16) + '\n' +
-                    address.toFriendly() + '\n' +
-                    amount.toString(10)
-                    document.getElementById('output').textContent = result;
+                    let destAddress = cellRef.readAddress().toFriendly();  // destation address
+                    let amount = cellRef.readCoins().toString(10)   // amount
+                    document.getElementById('flags').textContent = flags;
+                    document.getElementById('dest').textContent = destAddress;
+                    document.getElementById('amount').textContent = new BigNumber(amount).dividedBy('1e9').toFormat(4);
                 }
         fr.readAsArrayBuffer(file.current.files[0]);
     }, []);
@@ -38,7 +38,9 @@ function App() {
             <header className="App-header">
                 <div>
                     <input ref={file} type="file" onChange={onFileChange}></input>
-                    <pre id="output"></pre>
+                    <p>Flags: <pre id="flags"></pre></p>
+                    <p>Destation address: <pre id="dest"></pre></p>
+                    <p>Amount: <pre id="amount"></pre></p>
                 </div>
                 <p>
                     
