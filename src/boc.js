aee7fe93883bfc1ec34a0cc350245e2be17356a1
doron aviguy <doronaviguy@gmail.com>
2022-04-05 17:20:52 +0300

    fix the bounce bit for internal message

--- src/boc.js
+++ src/boc.js
@@ -27,14 +27,17 @@ export function readExternalMessageD(slice) {
     }
 }
 
-
+// wallet-v3.fif
+// <b b{01} s, bounce 1 i, b{000} s, dest_addr Addr, amount Gram+cc, 0 9 64 32 + + u,
+//   body-cell <s 2dup 1 s-fits-with? not rot over 1 i, -rot { drop body-cell ref, } { s, } cond
+// b>
 export function readInternalMessageD(slice) {
-    
+    slice.skip(1); 
     const ihrDisabled = slice.readUint(1).toNumber();
     const bounceFlag = slice.readUint(1).toNumber();
     const bouncedFlag = slice.readUint(1).toNumber();
-    slice.skip(3); //anycast address
-    const destination = slice.readAddress().toFriendly();  // destnation address
+    slice.skip(2); //anycast address
+    const destination = slice.readAddress().toFriendly();  // destination address
     const amount = slice.readCoins();   // amount
     return {
         ihrDisabled,
